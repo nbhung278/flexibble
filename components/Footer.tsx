38bcbd27376d97ade72dee9cba8489d5651ec692
nbhung278 <nbhung278@gmail.com>
2023-11-15 00:14:11 +0700

    changes logo path

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,7 +24,7 @@ const Footer = () => (
   <section className="flexStart footer">
     <div className="flex flex-col gap-12 w-full">
       <div className="flex items-start flex-col">
-        <Image src="/logo-purple.svg" width={116} height={38} alt="logo" />
+        <Image src="./logo-purple.svg" width={116} height={38} alt="logo" />
 
         <p className="text-start text-sm font-normal mt-5 max-w-xs">
           Flexibble is the world&apos;s leading community for creatives to
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = async () => {
     <nav className="flexBetween navbar">
       <div className="flex-1 flexStart gap-10">
         <Link href="/">
-          <Image src="/logo.svg" width={116} height={43} alt="logo" />
+          <Image src="./logo.svg" width={116} height={43} alt="logo" />
         </Link>
         <ul className="xl:flex hidden text-small gap-7">
           {NavLinks.map((link) => (
